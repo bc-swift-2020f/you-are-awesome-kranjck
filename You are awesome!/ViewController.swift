b6fa0dfc0d03c9525fff7a0536b333c17ac69f2d
Kristen Kranjc <kranjck@bc.edu>
2021-01-03 17:18:29 -0800

    Chapter 2.8—added conditionals

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -14,14 +14,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
-       
     }
-
-
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You are awesome!"
-        imageView.image = UIImage(named: "image0")
-        
+        let awesomeMessage = "You are awesome!"
+        let greatMessage = "You are great!"
+        let daBombMessage = "You are da bomb!"
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        }else if messageLabel.text == greatMessage {
+            messageLabel.text = daBombMessage
+            imageView.image = UIImage(named: "image2")
+        }else{
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
 }
 
